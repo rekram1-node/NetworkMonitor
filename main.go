62ccabc84affd3bbda55ad143839653dec3e40dc
Aiden Cline <aidenpcline@gmail.com>
2022-11-14 22:16:35 -0600

    update

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 
 func main() {
 	online := monitor.ConnectedToInternet()
-
 	if !online {
 		currentTime := time.Now()
 		fmt.Println("failed at " + currentTime.Format("2006-01-02 15:04:05"))
