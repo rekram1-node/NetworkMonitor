6c0ec378a6410afb9e27dc753c60a5d2e4a1fa49
Aiden Cline <aidenpcline@gmail.com>
2022-12-07 15:18:38 -0600

    fix unneccessary thing

--- main.go
+++ main.go
@@ -44,9 +44,9 @@ func main() {
 	}
 	logger.Info.Msg("Checking Connection...")
 	cmd, _ := exec.Command("ls").Output()
-	fmt.Println(cmd)
+	fmt.Println(string(cmd))
 	cmd, _ = exec.Command("pwd").Output()
-	fmt.Println(cmd)
+	fmt.Println(string(cmd))
 	// scheduled.ConnectionCheck(dir, timeLayout)
 }
 
